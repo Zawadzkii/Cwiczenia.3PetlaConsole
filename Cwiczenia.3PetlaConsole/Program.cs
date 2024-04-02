e304b402affddbe791c2f3212daf3fb5f331d8bf
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:21:10 +0200

    Add project files.

---
+++ Cwiczenia.3PetlaConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10; // liczba elementów ciągu do wygenerowania
+        Console.WriteLine("Ciąg Fibonacciego:");
+
+        // Wyświetlenie pierwszych n elementów ciągu Fibonacciego
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    // Metoda do obliczania n-tego elementu ciągu Fibonacciego
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
